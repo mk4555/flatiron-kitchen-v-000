0c64676058800e811997a20b6f959ce3bfe76e1d
Min Kim <mk4555@nyu.edu>
2018-02-19 18:08:29 +0000

    automatically backed up by learn

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -3,9 +3,16 @@ class RecipeController < ApplicationController
     @recipe = Recipe.new
   end
   def create
+
   end
   def edit
   end
   def update
+  end
+
+  private
+
+  def recipe_params
+    params.require(:recipe).permit(:name)
   end
 end
